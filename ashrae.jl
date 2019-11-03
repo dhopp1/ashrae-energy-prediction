b9be179d809849815b5221c74b7310d5d90e8d85
dhopp1 <daniel.hopp1@gmail.com>
2019-11-03 16:59:15 +0100

    Drop zero meter readings

--- ashrae.jl
+++ ashrae.jl
@@ -1,4 +1,3 @@
-
 using
 	CSV,
 	DataFrames,
@@ -15,8 +14,15 @@ using
 grid_search = true
 # whether to work with only a 500k sample of the data
 sampling = true
-# number of cores for distributed computing
-addprocs(4)
+# whether to read in the test data
+test_set = false
+# whether or not to do one hot encoding
+one_hot_encoding = false
+# whether or not to include 0 meter readings
+include_zero = false
+# if submitting, which algorithm
+submitting = "bst" # ["dt", "rf", "bst"]
+do_submission = false
 # historical performance of algorithms
 perf = CSV.file("data/performance.csv") |> DataFrame!
 
@@ -24,6 +30,9 @@ perf = CSV.file("data/performance.csv") |> DataFrame!
 # training data read
 building = CSV.file("data/building_metadata.csv") |> DataFrame!
 train = CSV.file("data/train.csv") |> DataFrame!
+if include_zero == false
+	train = train[train.meter_reading .> 0, :]
+end
 if sampling
 	train = train[sample(axes(train, 1), 500000; replace = false, ordered = true), :]
 end
@@ -40,17 +49,18 @@ full[!, :dow] = Dates.dayofweek.(full.timestamp)
 
 
 # test data read
-test = CSV.file("data/test.csv") |> DataFrame!
-weather_test = CSV.file("data/weather_test.csv") |> DataFrame!
-full_test = join(test, building, on=:building_id, kind=:left) |> x -> join(x, weather_test, on=[:site_id, :timestamp], kind=:left)
-# adding day, month, hour
-date_format = Dates.DateFormat("yyyy-mm-dd HH:MM:ss")
-full_test.timestamp = Dates.DateTime.(full_test.timestamp, date_format)
-full_test[!, :day] = Dates.day.(full_test.timestamp)
-full_test[!, :month] = Dates.month.(full_test.timestamp)
-full_test[!, :hour] = Dates.hour.(full_test.timestamp)
-full_test[!, :dow] = Dates.dayofweek.(full_test.timestamp)
-
+if test_set
+	test = CSV.file("data/test.csv") |> DataFrame!
+	weather_test = CSV.file("data/weather_test.csv") |> DataFrame!
+	full_test = join(test, building, on=:building_id, kind=:left) |> x -> join(x, weather_test, on=[:site_id, :timestamp], kind=:left)
+	# adding day, month, hour
+	date_format = Dates.DateFormat("yyyy-mm-dd HH:MM:ss")
+	full_test.timestamp = Dates.DateTime.(full_test.timestamp, date_format)
+	full_test[!, :day] = Dates.day.(full_test.timestamp)
+	full_test[!, :month] = Dates.month.(full_test.timestamp)
+	full_test[!, :hour] = Dates.hour.(full_test.timestamp)
+	full_test[!, :dow] = Dates.dayofweek.(full_test.timestamp)
+end
 
 ### functions
 # define one hot function
@@ -77,21 +87,23 @@ function result(pred, target)
 end
 
 # generate the dataset needed for algorithms from "full" dfs
-function gen_dataset(df, test_set=true)
+function gen_dataset(df, test_set=true, one_hot_encoding=one_hot_encoding)
 	X = copy(df)
 	# desired columns
 	if test_set
-		X = X[!, [:meter, :site_id, :square_feet, :year_built, :floor_count, :air_temperature, :cloud_coverage, :dew_temperature, :precip_depth_1_hr, :sea_level_pressure, :wind_direction, :wind_speed, :meter_reading, :day, :month, :hour, :dow]]
+		X = X[!, [:meter, :site_id, :square_feet, :year_built, :floor_count, :air_temperature, :cloud_coverage, :dew_temperature, :precip_depth_1_hr, :sea_level_pressure, :wind_direction, :wind_speed, :meter_reading, :month, :hour, :day, :dow]]
 	else
-		X = X[!, [:meter, :site_id, :square_feet, :year_built, :floor_count, :air_temperature, :cloud_coverage, :dew_temperature, :precip_depth_1_hr, :sea_level_pressure, :wind_direction, :wind_speed, :day, :month, :hour, :dow]]
+		X = X[!, [:meter, :site_id, :square_feet, :year_built, :floor_count, :air_temperature, :cloud_coverage, :dew_temperature, :precip_depth_1_hr, :sea_level_pressure, :wind_direction, :wind_speed, :month, :hour, :day, :dow]]
 	end
 	# filling nas with means
 	for col in names(X)[1:end-1]
 	    X[!, Symbol(col)] = recode(X[!, Symbol(col)], missing => mean(skipmissing(X[!, Symbol(col)])))
 	end
 	# one hot encoding
-	for col in [:meter, :site_id, :dow]
-		X = one_hot(X, col)
+	if one_hot_encoding
+		for col in [:meter, :site_id, :dow]
+			X = one_hot(X, col)
+		end
 	end
 	return X
 end
@@ -122,126 +134,139 @@ end
 # training data
 X = gen_dataset(full)
 X_train, X_test, y_train, y_test = train_test_split(X, "meter_reading")
-X_submission = gen_dataset(full_test, false)
+if test_set
+	X_submission = gen_dataset(full_test, false)
+end
 
 
 # simple decision tree
-if grid_search
-	performance = []
-	max_depth = [10, 50, 100, -1]
-	min_samples_split = [2, 5, 20, 50]
-	min_samples_leaf = [1, 5, 20, 50]
-	min_purity_increase = [0.0, 0.001, 0.01]
-	for md in max_depth
-		for mss in min_samples_split
-			for msl in min_samples_leaf
-				for mpi in min_purity_increase
-					params = "decision tree, max_depth:$md, min_samples_split:$mss, min_samples_leaf:$msl, min_purity_increase:$mpi"
+if submitting == "dt"
+	if grid_search
+		performance = []
+		max_depth = [10, 50, 100, -1]
+		min_samples_split = [2, 5, 20, 50]
+		min_samples_leaf = [1, 5, 20, 50]
+		min_purity_increase = [0.0, 0.001, 0.01]
+		for md in max_depth
+			for mss in min_samples_split
+				for msl in min_samples_leaf
+					for mpi in min_purity_increase
+						params = "decision tree, max_depth:$md, min_samples_split:$mss, min_samples_leaf:$msl, min_purity_increase:$mpi"
+						println(params)
+						dt = DecisionTreeRegressor(
+								max_depth = md,
+								min_samples_split = mss,
+								min_samples_leaf = msl,
+								min_purity_increase = mpi
+							)
+						DecisionTree.fit!(dt, convert(Matrix, X_train), convert(Array, y_train))
+						preds = DecisionTree.predict(dt, convert(Matrix, X_test))
+						push!(performance, (
+							params,
+							result(preds, y_test),
+							0,
+							"500k sample 80% of data, no one hot, no zero readings"
+						))
+					end
+				end
+			end
+		end
+		tmp_perf = DataFrame(performance)
+		names!(tmp_perf, [:desc, :val_score, :test_score, :notes])
+		perf = [perf; tmp_perf]
+		CSV.write("data/performance.csv", perf)
+		best_score = minimum([i[2] for i in performance])
+		best_params = [i[1] for i in performance if i[2] == best_score]
+		println(best_params)
+	end
+# submission
+	if do_submission
+		dt = DecisionTreeRegressor(
+			max_depth = -1,
+			min_samples_split = 2,
+			min_samples_leaf = 5,
+			min_purity_increase = 0.0
+		)
+		DecisionTree.fit!(dt, convert(Matrix, select(X, Not(:meter_reading))), X[!, :meter_reading])
+		final_preds = DecisionTree.predict(dt, convert(Matrix, X_submission))
+		X_submission[!, :meter_reading] = final_preds
+		X_submission[!, :row_id] = test.row_id
+		CSV.write("data/submission_dt.csv", X_submission[!, [:row_id, :meter_reading]])
+	end
+end
+
+
+# random forest
+if submitting == "rf"
+	n_subfeatures=-1; n_trees=20; partial_sampling=0.7; max_depth=-1
+	min_samples_leaf=5; min_samples_split=2; min_purity_increase=0.0
+	rf = build_forest(X[!, :meter_reading], convert(Matrix, select(X, Not(:meter_reading))),
+	                     n_subfeatures,
+	                     n_trees,
+	                     partial_sampling,
+	                     max_depth,
+	                     min_samples_leaf,
+	                     min_samples_split,
+	                     min_purity_increase)
+	#preds = apply_forest(rf, convert(Matrix, X_test))
+	#print("rf: " * results(preds, y_test))
+	# submission
+	if do_submission
+		final_preds = apply_forest(rf, convert(Matrix, X_submission))
+		X_submission[!, :meter_reading] = final_preds
+		X_submission[!, :row_id] = test.row_id
+		CSV.write("data/submission_rf.csv", X_submission[!, [:row_id, :meter_reading]])
+	end
+end
+
+
+# xgboost
+if submitting == "bst"
+	if grid_search
+		f(x) = maximum([x, 0])
+		performance = []
+		num_round = [10, 20, 30]
+		eta = [0.1]
+		max_depth = [50]
+		for nr in num_round
+			for et in eta
+				for md in max_depth
+					params = "xgboost, num_round:$nr, eta:$et, max_depth:$md,"
 					println(params)
-					dt = DecisionTreeRegressor(
-							max_depth = md,
-							min_samples_split = mss,
-							min_samples_leaf = msl,
-							min_purity_increase = mpi
+					bst = xgboost(
+							convert(Matrix, X_train),
+							nr,
+							label=convert(Array, y_train),
+							eta=et,
+							max_depth=md
 						)
-					DecisionTree.fit!(dt, convert(Matrix, X_train), convert(Array, y_train))
-					preds = DecisionTree.predict(dt, convert(Matrix, X_test))
+					preds = XGBoost.predict(bst, convert(Matrix, X_test))
 					push!(performance, (
 						params,
-						result(preds, y_test),
+						result(f.(preds), y_test),
 						0,
-						"500k sample 80% of data"
+						"500k sample 80% of data, no one hot"
 					))
 				end
 			end
 		end
+		tmp_perf = DataFrame(performance)
+		names!(tmp_perf, [:desc, :val_score, :test_score, :notes])
+		perf = [perf; tmp_perf]
+		CSV.write("data/performance.csv", perf)
+		best_score = minimum([i[2] for i in performance])
+		best_params = [i[1] for i in performance if i[2] == best_score]
+		println(best_params)
 	end
-	tmp_perf = DataFrame(performance)
-	names!(tmp_perf, [:desc, :val_score, :test_score, :notes])
-	perf = [perf; tmp_perf]
-	CSV.write("data/performance.csv", perf)
-	best_score = minimum([i[2] for i in performance])
-	best_params = [i[1] for i in performance if i[2] == best_score]
-	println(best_params)
-end
-# submission
-dt = DecisionTreeRegressor(
-	max_depth = 100,
-	min_samples_split = 5,
-	min_samples_leaf = 5,
-	min_purity_increase = .001
-)
-DecisionTree.fit!(dt, convert(Matrix, select(X, Not(:meter_reading))), X[!, :meter_reading])
-final_preds = DecisionTree.predict(dt, convert(Matrix, X_submission))
-X_submission[!, :meter_reading] = final_preds
-X_submission[!, :row_id] = test.row_id
-CSV.write("data/submission_dt.csv", X_submission[!, [:row_id, :meter_reading]])
 
-
-# random forest
-n_subfeatures=-1; n_trees=20; partial_sampling=0.7; max_depth=-1
-min_samples_leaf=5; min_samples_split=2; min_purity_increase=0.0
-rf = @distributed build_forest(X[!, :meter_reading], convert(Matrix, select(X, Not(:meter_reading))),
-                     n_subfeatures,
-                     n_trees,
-                     partial_sampling,
-                     max_depth,
-                     min_samples_leaf,
-                     min_samples_split,
-                     min_purity_increase)
-#preds = apply_forest(rf, convert(Matrix, X_test))
-#print("rf: " * results(preds, y_test))
-# submission
-final_preds = apply_forest(rf, convert(Matrix, X_submission))
-X_submission[!, :meter_reading] = final_preds
-X_submission[!, :row_id] = test.row_id
-CSV.write("data/submission_rf.csv", X_submission[!, [:row_id, :meter_reading]])
-
-
-# xgboost
-if grid_search
-	f(x) = maximum([x, 0])
-	performance = []
-	num_round = [10, 20, 30]
-	eta = [0.1]
-	max_depth = [50]
-	for nr in num_round
-		for et in eta
-			for md in max_depth
-				params = "xgboost, num_round:$nr, eta:$et, max_depth:$md,"
-				println(params)
-				bst = xgboost(
-						convert(Matrix, X_train),
-						nr,
-						label=convert(Array, y_train),
-						eta=et,
-						max_depth=md
-					)
-				preds = XGBoost.predict(bst, convert(Matrix, X_test))
-				push!(performance, (
-					params,
-					result(f.(preds), y_test),
-					0,
-					"500k sample 80% of data"
-				))
-			end
-		end
+	# submission
+	if do_submission
+		bst = xgboost(convert(Matrix, select(X, Not(:meter_reading))), 20, label=X[!, :meter_reading], eta=0.1, max_depth=50)
+		# bst = Booster(model_file = "models/xgb.model")
+		XGBoost.save(bst,"models/xgb.model")
+		final_preds = XGBoost.predict(bst, convert(Matrix, X_submission))
+		X_submission[!, :meter_reading] = final_preds
+		X_submission[!, :row_id] = test.row_id
+		CSV.write("data/submission_bst.csv", X_submission[!, [:row_id, :meter_reading]])
 	end
-	tmp_perf = DataFrame(performance)
-	names!(tmp_perf, [:desc, :val_score, :test_score, :notes])
-	perf = [perf; tmp_perf]
-	CSV.write("data/performance.csv", perf)
-	best_score = minimum([i[2] for i in performance])
-	best_params = [i[1] for i in performance if i[2] == best_score]
-	println(best_params)
 end
-
-# all data
-bst = xgboost(convert(Matrix, select(X, Not(:meter_reading))), 20, label=X[!, :meter_reading], eta=0.1, max_depth=50)
-# bst = Booster(model_file = "models/xgb.model")
-XGBoost.save(bst,"models/xgb.model")
-# submission
-final_preds = XGBoost.predict(bst, convert(Matrix, X_submission))
-X_submission[!, :meter_reading] = final_preds
-X_submission[!, :row_id] = test.row_id
-CSV.write("data/submission_bst.csv", X_submission[!, [:row_id, :meter_reading]])
